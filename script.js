555f77d15958327123e8f7755c2a506d4f32aaa5
Juveria Manzar <juveriamanzar29@gmail.com>
2021-07-01 15:49:12 +0530

    prioritising tickets

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 let addBtn = document.querySelector(".add");
 let body = document.querySelector("body");
 
-let grid=document.querySelector('.grid');
+let grid = document.querySelector('.grid');
 
 addBtn.addEventListener("click", function () {
 
-    let preModal=document.querySelector('.modal')
-    if(preModal!=null) return;
+    let preModal = document.querySelector('.modal')
+    if (preModal != null) return;
 
     let div = document.createElement("div");
     div.classList.add("modal");
-    div.innerHTML = 
-    `<div class="task-section">
+    div.innerHTML =
+        `<div class="task-section">
         <div class="task-inner-container" contenteditable="true"></div>
     </div>
     <div class="priority-section">
@@ -25,6 +25,7 @@ addBtn.addEventListener("click", function () {
     </div>`
 
     let ticketColor = "black"
+    let colors = ["pink", "blue", "green", "black"]
     let allFilters = div.querySelectorAll('.modal-priority');
     for (let i = 0; i < allFilters.length; i++) {
         allFilters[i].addEventListener('click', function (e) {
@@ -40,16 +41,33 @@ addBtn.addEventListener("click", function () {
     taskInnerContainer.addEventListener('keypress', function (e) {
         if (e.key == "Enter") {
 
-            let ticketDiv=document.createElement('div');
+            let ticketDiv = document.createElement('div');
             ticketDiv.classList.add('ticket')
 
-            ticketDiv.innerHTML=
-            `<div class="ticket-color ${ticketColor}"></div>
+            ticketDiv.innerHTML =
+                `<div class="ticket-color ${ticketColor}"></div>
             <div class="ticket-id">#aeid</div>
             <div class="actual-task">${e.currentTarget.innerText}</div>`
 
+            let ticketColorDiv = ticketDiv.querySelector('.ticket-color');
+            ticketColorDiv.addEventListener('click', function (e) {
+                let currColor = e.currentTarget.classList[1];
+
+                let index = -1
+                for (let i = 0; i < colors.length; i++) {
+                    if (colors[i] == currColor) index = i;
+                }
+
+                index++;
+                index=index%4;
+
+                let newColor=colors[index];
+
+                ticketColorDiv.classList.remove(currColor);
+                ticketColorDiv.classList.add(newColor);
+            });
             grid.append(ticketDiv)
-            div.remove(); 
+            div.remove();
         }
     })
 
